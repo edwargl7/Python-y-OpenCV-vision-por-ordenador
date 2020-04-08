7a428ac0b27108f8692d166ca08879b9923d59f3
Heiner Edwar Granados Leal <07edwargl@gmail.com>
2020-04-08 18:36:49 -0500

    Procesar Imagen a un array con numpy

---
+++ procesamiento_imagenes_con_arrays.py
@@ -0,0 +1,33 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image
+
+# Cargar imagen
+imagen_perro = Image.open('/home/heiner-edwar/Imágenes/perro.jpg')
+imagen_perro.show()
+print(type(imagen_perro))
+print("Tamaño imagen: {}".format(imagen_perro.size))
+
+# Convertir imagen en array
+array_image = np.asarray(imagen_perro)
+
+# Plotear arreglo
+plot_general = plt.subplot(2, 2, 1)
+plt.imshow(array_image)
+plot_general.set_title('General')
+print("Tamaño array: {}".format(array_image.shape))
+
+plot_red = plt.subplot(2, 2, 2)
+plt.imshow(array_image[:, :, 0])
+plot_red.set_title('Red')
+
+plot_green = plt.subplot(2, 2, 3)
+plt.imshow(array_image[:, :, 1])
+plot_green.set_title('Green')
+
+plot_blue = plt.subplot(2, 2, 4)
+plt.imshow(array_image[:, :, 2])
+plot_blue.set_title('Blue')
+
+plt.tight_layout()
+plt.show()
